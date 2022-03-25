7caf9b04a1a1ff0a70138bff5b76e089b35358a5
jago.gif <halcon1411@gmail.com>
2022-03-25 10:47:00 -0300

    simple piedra, papel o tijeras

---
+++ src/cl/piedraPapalOTijeras/juego.java
@@ -0,0 +1,33 @@
+package cl.piedraPapalOTijeras;
+
+import java.util.Scanner;
+
+public class juego {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int juego = 0;
+        int jugada;
+        for (int i = 0; i < 10; i++) {
+            int maquina = (int) (Math.random() * 3 + 1);
+            System.out.println("piedra=[1] palel=[2] o tijeras=[3]");
+            jugada = scan.nextInt();
+            if (jugada > maquina) {
+                System.out.println("ganaste 1 punto");
+                juego += 1;
+                System.out.println(juego);
+            }else{
+                System.out.println("perdiste 1 punto");
+                juego -= 1;
+                System.out.println(juego);
+            }
+        }
+        if (juego < 1){
+            System.out.println(" Perdiste... te falta practica");
+        }else if (juego==0){
+            System.out.println("Que pelea, quedaste empatado");
+        }else {
+            System.out.println("Felicidades ganaste");
+        }
+    }
+}
+
