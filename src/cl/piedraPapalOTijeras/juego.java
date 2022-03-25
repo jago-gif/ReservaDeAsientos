f92a19b4d2f4aac420c96e66593c26e2bb256c78
jago.gif <halcon1411@gmail.com>
2022-03-25 11:17:45 -0300

    correccion de sub menú ganador

--- src/cl/piedraPapalOTijeras/juego.java
+++ src/cl/piedraPapalOTijeras/juego.java
@@ -11,16 +11,38 @@ public class juego {
             int maquina = (int) (Math.random() * 3 + 1);
             System.out.println("piedra=[1] palel=[2] o tijeras=[3]");
             jugada = scan.nextInt();
-            if (jugada > maquina) {
-                System.out.println("ganaste 1 punto");
-                juego += 1;
-                System.out.println(juego);
-            }else{
-                System.out.println("perdiste 1 punto");
-                juego -= 1;
-                System.out.println(juego);
+            if (jugada == 1) {
+                if (maquina == 1){
+                    System.out.println("Piedra con piedra empatan");
+                }else if (maquina==2){
+                    System.out.println("Perdiste-- piedra pierde contra papel");
+                    juego -= 1;
+                }else {
+                    System.out.println("Ganaste--Piedra rompe tijeras");
+                    juego += 1;
+                }
+            }else if (jugada==2){
+                if (maquina == 1){
+                    System.out.println("Ganaste--papel gana contra piedra");
+                    juego += 1;
+                }else if (maquina==2){
+                    System.out.println("Papel con papel empatan");
+                }else {
+                    System.out.println("Perdiste--tijeras corta papel");
+                    juego -= 1;
+                }
+            }else {
+                if (maquina == 1){
+                    System.out.println("Perdiste--Tijera pierde contra piedra");
+                    juego -= 1;
+                }else if (maquina==2){
+                    System.out.println("Ganaste--tijera corta papel");
+                    juego += 1;
+                }else {
+                    System.out.println("tijera empata con tijera");
+                }
+                }
             }
-        }
         if (juego < 0){
             System.out.println(" Perdiste... te falta practica");
         }else if (juego >0) {
@@ -29,5 +51,6 @@ public class juego {
             System.out.println("que pelea, quedaron empatados");
         }
     }
+
 }
 
